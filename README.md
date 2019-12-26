6a250c2099ec664b2c93bc573646cd024393af03
yinchunyu <542968439@qq.com>
2019-12-26 15:22:15 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# type-gverify
+
+typescript 实现验证码插件
+
+## 安装下载
+
+- 下载地址 https://github.com/yinMrsir/type-gverify/releases
+
+- `npm i type-gverify`
+
+## 快速使用
+```
+import GVerify from 'type-gverify';
+
+verifyCode = new GVerify({
+    id: 'v_container',
+});
+
+if ((verifyCode).validate('')) {
+    console.log('验证通过');
+}
+
+html:
+<div id="v_container"></div>
+```
+
+## 参数说明
+<table>
+  <thead>
+    <tr>
+      <th>参数</th>
+      <th>说明</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>id</td>
+      <td>容器Id</td>
+    </tr>
+    <tr>
+	  <td>canvasId</td>
+	  <td>canvas的ID</td>
+	</tr>
+	<tr>
+      <td>width</td>
+      <td>canvas宽度</td>
+    </tr>
+    <tr>
+      <td>height</td>
+      <td>canvas高度</td>
+    </tr>
+    <tr>
+      <td>type</td>
+      <td>blend:数字字母混合类型 | number:纯数字 | letter:纯字母</td>
+    </tr>
+    <tr>
+      <td>size</td>
+      <td>设置验证码长度</td>
+    </tr>
+  </tbody>
+</table>
---
+++ index.ts
@@ -0,0 +1,164 @@
+interface OptionsState {
+  id: string;
+  canvasId: string;
+  width: string;
+  height: string;
+  type: string;
+  size: number;
+  code: string;
+  numArr: string[];
+  letterArr: string[];
+
+  [key: string]: any;
+}
+
+interface ParamsOptions {
+  id: string;
+  canvasId?: string;
+  width?: string;
+  height?: string;
+  type?: 'blend' | 'number' | 'letter'; // 图形验证码默认类型blend:数字字母混合类型、number:纯数字、letter:纯字母
+  size?: number;
+
+  [key: string]: any;
+}
+
+export default class GVerify {
+  private options: OptionsState = {
+    id: '',
+    canvasId: 'verifyCanvas',
+    width: '130',
+    height: '38',
+    type: 'blend',
+    code: '',
+    numArr: [],
+    letterArr: [],
+    size: 4,
+  };
+
+  constructor(options: ParamsOptions) {
+    let key: string;
+    for (key of Object.keys(options)) {
+      this.options[key] = options[key];
+    }
+    this.options.numArr = '0,1,2,3,4,5,6,7,8,9'.split(',');
+    this.options.letterArr = this.getAllLetter();
+
+    this.init();
+    this.refresh();
+  }
+
+  private getAllLetter(): string[] {
+    const letterStr: string = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
+    return letterStr.split(',');
+  }
+
+  private init() {
+    const con = document.getElementById(this.options.id);
+    const canvas = document.createElement('canvas');
+    canvas.id = this.options.canvasId;
+    canvas.width = Number(this.options.width);
+    canvas.height = Number(this.options.height);
+    canvas.style.cursor = 'pointer';
+    canvas.innerHTML = '您的浏览器版本不支持canvas';
+    (con as HTMLElement).appendChild(canvas);
+    const parent = this;
+    canvas.onclick = () => {
+      parent.refresh();
+    };
+  }
+
+  public validate(code: string) {
+    const CCode = code.toLowerCase();
+    const vCode = this.options.code.toLowerCase();
+    if (CCode === vCode) {
+      return true;
+    } else {
+      this.refresh();
+      return false;
+    }
+  }
+
+  private refresh() {
+    this.options.code = '';
+    const canvas = document.getElementById(this.options.canvasId);
+    let ctx;
+    if ((canvas as HTMLCanvasElement).getContext) {
+      ctx = (canvas as HTMLCanvasElement).getContext('2d');
+    } else {
+      return;
+    }
+
+    (ctx as CanvasRenderingContext2D).textBaseline = 'middle';
+    (ctx as CanvasRenderingContext2D).fillStyle = this.randomColor(180, 240);
+    (ctx as CanvasRenderingContext2D).fillRect(0, 0, Number(this.options.width), Number(this.options.height));
+
+    let txtArr: string[] = [];
+    if (this.options.type === 'blend') {
+      txtArr = this.options.numArr.concat(this.options.letterArr);
+    } else if (this.options.type === 'number') {
+      txtArr = this.options.numArr;
+    } else {
+      txtArr = this.options.letterArr;
+    }
+
+    for (let i = 1; i <= this.options.size; i++) {
+      const txt = txtArr[this.randomNum(0, txtArr.length)];
+      this.options.code += txt;
+      (ctx as CanvasRenderingContext2D).font = this.randomNum(
+        Number(this.options.height),
+        Number(this.options.height)) + 'px SimHei'; // 随机生成字体大小
+      // ctx.font = randomNum(this.options.height/2, this.options.height) + 'px SimHei'; //随机生成字体大小
+      (ctx as CanvasRenderingContext2D).fillStyle = this.randomColor(50, 160); // 随机生成字体颜色
+      (ctx as CanvasRenderingContext2D).shadowOffsetX = this.randomNum(-3, 3);
+      (ctx as CanvasRenderingContext2D).shadowOffsetY = this.randomNum(-3, 3);
+      (ctx as CanvasRenderingContext2D).shadowBlur = this.randomNum(-3, 3);
+      (ctx as CanvasRenderingContext2D).shadowColor = 'rgba(0, 0, 0, 0.3)';
+      const x = Number(this.options.width) / (this.options.size + 1) * i;
+      const y = Number(this.options.height) / 2;
+      const deg = this.randomNum(-30, 30);
+      /* 设置旋转角度和坐标原点**/
+      (ctx as CanvasRenderingContext2D).translate(x, y);
+      (ctx as CanvasRenderingContext2D).rotate(deg * Math.PI / 180);
+      (ctx as CanvasRenderingContext2D).fillText(txt, 0, 0);
+      /* 恢复旋转角度和坐标原点**/
+      (ctx as CanvasRenderingContext2D).rotate(-deg * Math.PI / 180);
+      (ctx as CanvasRenderingContext2D).translate(-x, -y);
+    }
+    /* 绘制干扰线**/
+    for (let i = 0; i < 4; i++) {
+      (ctx as CanvasRenderingContext2D).strokeStyle = this.randomColor(40, 180);
+      (ctx as CanvasRenderingContext2D).beginPath();
+      (ctx as CanvasRenderingContext2D)
+        .moveTo(this.randomNum(0, Number(this.options.width)), this.randomNum(0, Number(this.options.height)));
+      (ctx as CanvasRenderingContext2D)
+        .lineTo(this.randomNum(0, Number(this.options.width)), this.randomNum(0, Number(this.options.height)));
+      (ctx as CanvasRenderingContext2D).stroke();
+    }
+    /* 绘制干扰点**/
+    for (let i = 0; i < Number(this.options.width) / 4; i++) {
+      (ctx as CanvasRenderingContext2D).fillStyle = this.randomColor(0, 255);
+      (ctx as CanvasRenderingContext2D).beginPath();
+      (ctx as CanvasRenderingContext2D)
+        .arc(
+          this.randomNum(0, Number(this.options.width)),
+          this.randomNum(0, Number(this.options.height)),
+          1,
+          0,
+          2 * Math.PI);
+      (ctx as CanvasRenderingContext2D).fill();
+    }
+  }
+
+  private randomNum(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  private randomColor(min: number, max: number) {
+    const r = this.randomNum(min, max);
+    const g = this.randomNum(min, max);
+    const b = this.randomNum(min, max);
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+  }
+
+}
